168c7b658154d70dd21d6155436fdeb5c9627446
i.tuev <sadiolem.sds@gmail.com>
2022-12-25 18:17:44 +0300

    icons & card styles

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -5,15 +5,15 @@ function HeaderNav() {
   return (
     <div className={styles['header-nav']}>
       <button type="button" className={styles['cart-button']}>
-        <img src="./img/ui-icons/cart.svg" alt="cart" />
+        <img src="./img/ui-icons/EmojiFlatShoppingCart.svg" height={20} width={20} alt="cart" />
         1205 Руб.
       </button>
 
       <NavLink to="/favorites">
-        <img src="./img/ui-icons/favorites.svg" height={20} width={20} alt="favorites" />
+        <img src="./img/ui-icons/EmojiFlatWhiteHeart.svg" height={20} width={20} alt="favorites" />
       </NavLink>
       <NavLink to="/profile">
-        <img src="./img/ui-icons/profile.svg" height={20} width={20} alt="profile" />
+        <img src="./img/ui-icons/EmojiFlatShoppingBags.svg" height={20} width={20} alt="profile" />
       </NavLink>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App';
 import './assets/styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>
 );
